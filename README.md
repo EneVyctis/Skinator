7715b4bd2b0e9e2a29fde41e5134a270b0369959
Basile <basile.roux@telecom-sudparis.eu>
2023-09-21 10:09:53 +0200

    database up + some entity

---
+++ README.md
@@ -0,0 +1,20 @@
+## Important
+
+Notice that this little web app is a 25 hours student project. The main objective of this project : learning php with symfony.
+Hence, I do **not** garantee the security of the app.
+
+
+
+### Skinator
+
+With skinator, players can add their skin to their collection. 
+They can all create sub-collection called wardrobe to show them to the world, this may help people to sell or exchange skins for exemple (exchange system is not implemented in the ap tho). A ranking system might be implements along with its leaderboard. Items of each rarity will value a certain amount of points, your rank will follow your points.
+
+
+Unique  	 +500 points
+Legendary  +100 points 
+Epique     +50  points
+rare			 +5   points
+common     +0   points
+
+
---
+++ src/Entity/Wardrobe.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WardrobeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WardrobeRepository::class)]
+class Wardrobe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $isPublic = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isIsPublic(): ?bool
+    {
+        return $this->isPublic;
+    }
+
+    public function setIsPublic(bool $isPublic): static
+    {
+        $this->isPublic = $isPublic;
+
+        return $this;
+    }
+}
