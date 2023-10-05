7ad97483da4de6f172929b10a4b40b9c209690f0
Basile <basile.roux@telecom-sudparis.eu>
2023-10-05 10:57:32 +0200

    Switched all pages to twig's templates

--- src/Controller/WardrobeController.php
+++ src/Controller/WardrobeController.php
@@ -23,39 +23,16 @@ class WardrobeController extends AbstractController
      * List all wardrobe entities.
      */
 
-    #[Route('/list', name: 'wardrobe_list', methods: ['GET'])]
+    #[Route('wardrobe/list', name: 'wardrobe_list', methods: ['GET'])]
     #[Route('/index', name: 'wardrobe_index', methods: ['GET'])]
     public function listWardrobes(ManagerRegistry $doctrine)
     {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Wardrobe list!</title>
-    </head>
-    <body>
-        <h1>wardrobe list</h1>
-        <p>Here are all your wardrobe:</p>
-        <ul>';
         
         $entityManager= $doctrine->getManager();
         $wardrobes = $entityManager->getRepository(Wardrobe::class)->findAll();
-        foreach($wardrobes as $wardrobe) {
-            $url = $this->generateUrl(
-                'wardrobe_show',
-                ['id' => $wardrobe->getId()]);
-            $htmlpage .= '<li>
-            <a href="'. $url .'">'. $wardrobe->getName() .'</a></li>';
-         }
-        $htmlpage .= '</ul>';
-
-        $htmlpage .= '</body></html>';
-        
-        return new Response(
-            $htmlpage,
-            Response::HTTP_OK,
-            array('content-type' => 'text/html')
-            );
+        return $this->render('wardrobe/wardrobeList.html.twig',
+                [ 'wardrobes' => $wardrobes ]
+                );
     }
      /**
      * Finds and displays a todo entity.
@@ -63,27 +40,6 @@ class WardrobeController extends AbstractController
     #[Route('/{id}', name: 'wardrobe_show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function showAction(Wardrobe $wardrobe): Response
     {
-        $htmlpage = '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>wardrobe n° '.$wardrobe->getId().' details</title>
-    </head>
-    <body>
-        <h2>Wardrobe Details :</h2>
-        <ul>
-        <dl>';
-        
-        $htmlpage .= '<dt>Wardrobe</dt><dd>' . $wardrobe->getName() . '</dd>';
-        $htmlpage .= '<dt>Description</dt> <dd> ' . $wardrobe->getDescription() . '</dd>';
-        $htmlpage .= '<dt>Wardrobe of</dt> <dd> '. $wardrobe->getOwner() . '</dd>';
-        $htmlpage .= '</dl>';
-        $htmlpage .= '</ul></body></html>';
-                
-        return new Response(
-                $htmlpage,
-                Response::HTTP_OK,
-                array('content-type' => 'text/html')
-                );
+        return $this->render('wardrobe/showWardrobe.html.twig', [ 'wardrobe' => $wardrobe]);
     }
 }
